bd2f9b968400ca51fec52010dd8beb54f9795ac2
Oliver <oli.kern@hotmail.com>
2024-02-12 13:19:53 +0100

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "oamk_mobileNative_assignment7_oliver_kern"
+include(":app")
+ 
